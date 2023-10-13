827660bb5032c671bcb4f62db0940e1c837fc908
Manuel Palumbo <manuel.palumbo@email.com>
2023-10-13 10:26:57 +0200

    Fix: JavaDoc

--- src/main/java/ml/empee/simplemenu/model/masks/GapMask.java
+++ src/main/java/ml/empee/simplemenu/model/masks/GapMask.java
@@ -43,7 +43,7 @@ public class GapMask implements Mask {
   }
 
   /**
-   * @{inheritDoc}
+   * {@inheritDoc}
    */
   public <K> List<K> apply(List<K> items) {
     var result = new ArrayList<K>();
